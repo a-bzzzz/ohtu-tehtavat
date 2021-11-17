a305d2b14484e01ec0985c635c1402acf9fb893a
Asta Björk <asta.bjork@helsinki.fi>
2021-11-17 18:14:30 +0200

    Modified nhl-reader project, course tasks 1-2

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,19 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, assists, goals, penalties, team, games):
         self.name = name
+        self.nationality = nationality
+        self.assists = assists
+        self.goals = goals
+        self.penalties = penalties
+        self.team = team
+        self.games = games
+        self.points = self.assists + self.goals
     
     def __str__(self):
-        return self.name
+        return (
+            f"{self.name:20}"
+            f"\t{self.team:5}"
+            f"{(self.goals):2}"
+            f" + {(self.assists):2}"
+            f" = {(self.points):2}"
+        )
