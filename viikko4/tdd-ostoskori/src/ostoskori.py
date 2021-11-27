7ecc0c884cb3ca446d2599bdf33070771ff4d565
Asta Björk <asta.bjork@helsinki.fi>
2021-11-27 22:54:34 +0200

    3. Yhden tuotteen lisäämisen jälkeen ostoskorin hinta on sama kuin tuotteen hinta.

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -4,20 +4,25 @@ from ostos import Ostos
 class Ostoskori:
     def __init__(self):
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
-        self.ostoskori = []
+        self._ostoskori = []
+        self._hinta = 0
 
     def tavaroita_korissa(self):
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
-        return len(self.ostoskori)
+        return len(self._ostoskori)
 
     def hinta(self):
-        return 0
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
+        return self._hinta
 
     def lisaa_tuote(self, lisattava: Tuote):
-        self.ostoskori.append(Ostos(lisattava))
+        # lisää tuotteen
+        self._ostoskori.append(Ostos(lisattava))
+        tuotehinta = lisattava.hinta()
+        self._hinta += tuotehinta
+        return self._hinta
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
