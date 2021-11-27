ce78180409f7e4983c6e4f25fe8b59e4c5a3b79f
Asta Björk <asta.bjork@helsinki.fi>
2021-11-28 01:59:33 +0200

    Refactored ostoskori_tests.py

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,6 +5,8 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.maito = Tuote("Maito", 3)
+        self.makkara = Tuote("Makkara", 5)
 
     # step 1
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
@@ -13,98 +15,78 @@ class TestOstoskori(unittest.TestCase):
 
     # step 2
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
 
     # step 3
     def test_yhden_tuotteen_lisaamisen_jalkeen_korin_hinta_on_tuotteen_hinta(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.hinta(), 3)
 
     # step 4
     def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        makkara = Tuote("Makkara", 5)
-        self.kori.lisaa_tuote(makkara)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.makkara)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
     # step 5
     def test_kahden_tuotteen_lisaamisen_jalkeen_korin_hinta_on_kahden_tuotteen_hinta(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        makkara = Tuote("Makkara", 5)
-        self.kori.lisaa_tuote(makkara)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.makkara)
         self.assertEqual(self.kori.hinta(), 8)
 
     # step 6
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
     # step 7
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korin_hinta_on_kahden_tuotteen_hinta(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.hinta(), 6)
 
     # step 8
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
 
     # step 9
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio_jolla_oikea_tuotteen_nimi_ja_maara(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
 
     # step 10
     def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosoliota(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        makkara = Tuote("Makkara", 5)
-        self.kori.lisaa_tuote(makkara)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.makkara)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 2)
 
     # step 11
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
 
     # step 12
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_ostosolio_jolla_oikea_tuotteen_nimi_ja_maara(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 2)
 
     # step 13
     def test_korissa_kaksi_samaa_tuotetta_ja_poiston_jalkeen_yksi_ostos_jossa_yksi_tuote(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        self.kori.poista_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.poista_tuote(self.maito)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
         ostos = self.kori.ostokset()[0]
@@ -113,18 +95,15 @@ class TestOstoskori(unittest.TestCase):
 
     # step 14
     def test_korissa_yksi_tuote_ja_poiston_jalkeen_tyhja_ostoskori(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        self.kori.poista_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.poista_tuote(self.maito)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 0)
 
     # step 15
     def test_tyhjenna_metodi_tyhjentaa_ostoskorin(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        makkara = Tuote("Makkara", 5)
-        self.kori.lisaa_tuote(makkara)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.makkara)
         self.kori.tyhjenna()
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 0)
